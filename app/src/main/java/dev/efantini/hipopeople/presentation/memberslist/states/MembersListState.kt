370b6d43174fcc17dcaf994e9f49c2848c1aa36c
PizzaMarinara <pizzamarinara@protonmail.com>
2022-05-15 16:56:44 +0200

    Small fix in default values

--- app/src/main/java/dev/efantini/hipopeople/presentation/memberslist/states/MembersListState.kt
+++ app/src/main/java/dev/efantini/hipopeople/presentation/memberslist/states/MembersListState.kt
@@ -3,7 +3,7 @@ package dev.efantini.hipopeople.presentation.memberslist.states
 import dev.efantini.hipopeople.domain.model.Member
 
 data class MembersListState(
-    val loading: Boolean = true,
+    val loading: Boolean = false,
     val error: String = "",
     val members: List<Member> = listOf()
 )
