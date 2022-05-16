fd4d02e1397f31390a84effdd6a33f7f659cba13
PizzaMarinara <pizzamarinara@protonmail.com>
2022-05-16 12:45:20 +0200

    Filter ignores case

--- app/src/main/java/dev/efantini/hipopeople/shared/Extensions.kt
+++ app/src/main/java/dev/efantini/hipopeople/shared/Extensions.kt
@@ -4,9 +4,9 @@ import dev.efantini.hipopeople.domain.model.Member
 
 fun Member.filter(query: String): Boolean {
     return when {
-        this.name.contains(query) -> true
-        this.github.contains(query) -> true
-        this.hipo.position.contains(query) -> true
+        this.name.contains(query, ignoreCase = true) -> true
+        this.github.contains(query, ignoreCase = true) -> true
+        this.hipo.position.contains(query, ignoreCase = true) -> true
         else -> false
     }
 }
